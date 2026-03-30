f7576f6847af40e2c3d267da0af665e1adbb6f0b
Maximilian Hänsel <max@bluebatch.io>
2026-03-30 07:51:10 +0000

    full url block

--- app/robots.ts
+++ app/robots.ts
@@ -4,17 +4,11 @@ import { collectRewriteRules } from "@/lib/get-rewrites";
 export default function robots(): MetadataRoute.Robots {
   const { disallowPaths } = collectRewriteRules();
 
-  // For each disallowed path, allow its subpages (longer prefix wins in robots.txt)
-  const allowPaths = [
-    "/",
-    ...disallowPaths.map((p) => `${p}/`),
-  ];
-
   return {
     rules: {
       userAgent: "*",
-      allow: allowPaths,
-      disallow: disallowPaths,
+      allow: "/",
+      disallow: disallowPaths.map((p) => `${p}$`),
     },
     sitemap: "https://bluebatch.io/sitemap.xml",
   };
